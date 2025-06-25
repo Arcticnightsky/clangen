9e48cd7f0b6f5d92de53ee4795d766a8f9ebcdbf
Arcticnightsky <fralow.msn@gmail.com>
2025-06-25 12:13:02 -0400

    Update MedDenScreen.py

--- scripts/screens/MedDenScreen.py
+++ scripts/screens/MedDenScreen.py
@@ -347,9 +347,7 @@ class MedDenScreen(Screens):
             med_messages = []
 
             amount_per_med = get_amount_cat_for_one_medic(game.clan)
-            number = medical_cats_condition_fulfilled(
-                Cat.all_cats.values(), amount_per_med, give_clanmembers_covered=True
-            )
+            number = amount_clanmembers_covered(Cat.all_cats.values(), amount_per_med)
 
             meds_cover = i18n.t(
                 "screens.med_den.meds_cover",
@@ -564,7 +562,7 @@ class MedDenScreen(Screens):
                         condition_list.extend(
                             [
                                 i18n.t(f"conditions.permanent_conditions.{permcond}")
-                                for permcond in list(cat.injuries.keys())
+                                for permcond in list(cat.permanent_condition.keys())
                             ]
                         )
             conditions = ",<br>".join(condition_list)
