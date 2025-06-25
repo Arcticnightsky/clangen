235a49731dfe19fc6bd83c9368f1c882f5b0e6e2
Arcticnightsky <fralow.msn@gmail.com>
2025-06-25 14:06:42 -0400

    Update MedDenScreen.py

--- scripts/screens/MedDenScreen.py
+++ scripts/screens/MedDenScreen.py
@@ -619,8 +619,7 @@ class MedDenScreen(Screens):
                 "",
                 object_id="#med_cat_den_hover",
                 tool_tip_text=herb_display,
-                manager=MANAGER,
-                starting_height=2,  # <-- important for tooltip to appear
+                manager=MANAGER,  # <-- important for tooltip to appear
             )
         else:
             count = 1
@@ -653,8 +652,7 @@ class MedDenScreen(Screens):
                 "",
                 object_id="#med_cat_den_hover_big",
                 tool_tip_text=herb_display,
-                manager=MANAGER,
-                starting_height=2,  # <-- important for tooltip to appear
+                manager=MANAGER,  # <-- important for tooltip to appear
             )
 
         # otherwise draw the herbs you have
@@ -695,6 +693,7 @@ class MedDenScreen(Screens):
                     (792, 448),
                 ),
                 manager=MANAGER,
+                starting_height=2,
             )
 
     def exit_screen(self):
