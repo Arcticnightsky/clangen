4b4514e00c47aa70e236323cced945e4664bcad8
Arcticnightsky <fralow.msn@gmail.com>
2025-07-02 13:09:37 -0400

    Update MedDenScreen.py

--- scripts/screens/MedDenScreen.py
+++ scripts/screens/MedDenScreen.py
@@ -619,7 +619,7 @@ class MedDenScreen(Screens):
                 "",
                 object_id="#med_cat_den_hover",
                 tool_tip_text=herb_display,
-                starting_height=2
+                starting_height=2,
                 manager=MANAGER,
             )
         else:
@@ -652,7 +652,7 @@ class MedDenScreen(Screens):
                 "",
                 object_id="#med_cat_den_hover_big",
                 tool_tip_text=herb_display,
-                starting_height=2
+                starting_height=2,
                 manager=MANAGER,
             )
 
