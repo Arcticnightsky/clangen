39bc1dfaaff2d034ba94d311d582d9879c7cea7a
Arcticnightsky <fralow.msn@gmail.com>
2025-07-09 18:32:38 -0400

    Update MedDenScreen.py

--- scripts/screens/MedDenScreen.py
+++ scripts/screens/MedDenScreen.py
@@ -681,6 +681,7 @@ class MedDenScreen(Screens):
                 ),
                 tool_tip_text=herb_display,
                 manager=MANAGER,
+                starting_height=2,
             )
 
         # Draw the med den rock *after* herbs so it appears behind
