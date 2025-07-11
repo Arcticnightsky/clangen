3e31b3543e8dc9e60b767567fbd1ff06af54f6c1
Arcticnightsky <fralow.msn@gmail.com>
2025-07-11 14:29:07 -0400

    Update MedDenScreen.py

--- scripts/screens/MedDenScreen.py
+++ scripts/screens/MedDenScreen.py
@@ -718,7 +718,6 @@ class MedDenScreen(Screens):
         self.meds_messages.kill()
         self.last_med.kill()
         self.next_med.kill()
-        self.den_base.kill()
         for herb in self.herbs:
             self.herbs[herb].kill()
         self.herbs = {}
