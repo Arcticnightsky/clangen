0a310c916abea7dbd9e72475f0765b0a9c68d4cf
Arcticnightsky <fralow.msn@gmail.com>
2025-08-01 23:57:50 -0400

    Update cats.py

--- scripts/cat/cats.py
+++ scripts/cat/cats.py
@@ -626,7 +626,7 @@ class Cat:
         self.pelt.rebuild_sprite = True
 
         # exiled cats are special, cus they get kicked out a heaven
-        if self.status.is_exiled(CatGroup.PLAYER_CLAN) and not CatGroup("near"):
+        if CatStanding.EXILED:
             self.status.add_to_group(CatGroup.DARK_FOREST)
 
     def exile(self):
