a4d9ae4ec8e51f66e6df63c4a64ba2f8ce5aa7ad
Arcticnightsky <fralow.msn@gmail.com>
2025-08-02 01:11:13 -0400

    Update pelts.py

--- scripts/cat/pelts.py
+++ scripts/cat/pelts.py
@@ -1002,14 +1002,14 @@ class Pelt:
             self.name = selected.name
             self.length = selected.length
 # Force male tortie check here
-            if self.gender == "male" and self.tortiebase in Pelt.torties:
+            if cat.gender == "male" and cat.tortiebase in Pelt.torties:
                 boosted_inheritance = max(200, constants.CONFIG["cat_generation"]["direct_inheritance"] + 49)
                 if random.randint(1, boosted_inheritance) != 1:
                     print(f"Converting rare male tortie kit '{kit.name}' to female for realism.")
-                    self.gender = "female"
+                    cat.gender = "female"
                     self.tortiebase = selected.tortiebase
                 else: 
-                    self.gender = "male"
+                    cat.gender = "male"
             self.colour = selected.colour
     # If selected is a tortie and the kit is male, apply extra rarity
     # Safe to inherit (either not a tortie, or passed rare check)
