1097ee18d32f0c53da7c8f3294a8fbcc1dce411e
Arcticnightsky <fralow.msn@gmail.com>
2025-08-13 14:51:31 -0400

    Update handle_short_events.py

--- scripts/events_module/short/handle_short_events.py
+++ scripts/events_module/short/handle_short_events.py
@@ -112,6 +112,15 @@ class HandleShortEvents:
         self.random_cat = random_cat
         self.victim_cat = victim_cat
 
+
+        # Prevent random cat from being the same as main cat by reshuffling
+        if self.random_cat == self.main_cat:
+            possible_cats = [
+                c for c in Cat.all_cats.values()
+                if c.status.alive_in_player_clan and c != self.main_cat
+            ]
+            self.random_cat = random.choice(possible_cats) if possible_cats else None
+            
         # random cat gets added to involved later on, only if the event chosen requires a random cat
         self.involved_cats = [self.main_cat.ID]
 
