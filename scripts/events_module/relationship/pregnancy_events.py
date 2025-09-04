cfd8277ea8a74c7194060febb5ad239a20431968
Arcticnightsky <fralow.msn@gmail.com>
2025-09-03 21:40:12 -0400

    Update pregnancy_events.py

--- scripts/events_module/relationship/pregnancy_events.py
+++ scripts/events_module/relationship/pregnancy_events.py
@@ -1116,7 +1116,7 @@ class Pregnancy_Events:
         # Now that the second parent is determined, we can calculate the balanced chance for kits
         # get the chance for pregnancy
         if kits_are_adopted:
-            if len(first_parent.mate) > 0 and not affair: 
+            if len(first_parent.mate) > 0 and not affair:
                 inverse_chance = constants.CONFIG["pregnancy"]["primary_chance_same_sex_adoption"]
         else:
             inverse_chance = constants.CONFIG["pregnancy"]["primary_chance_unmated"]
