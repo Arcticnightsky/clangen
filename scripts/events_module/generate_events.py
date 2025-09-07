ed8c221b2e18716595805c276f55621a66e2b93f
Arcticnightsky <fralow.msn@gmail.com>
2025-09-07 12:05:25 -0400

    Update generate_events.py

--- scripts/events_module/generate_events.py
+++ scripts/events_module/generate_events.py
@@ -5,6 +5,7 @@ import random
 import i18n
 import ujson
 
+from scripts.cat.skills import SkillPath
 from scripts.cat_relations.enums import RelType
 from scripts.events_module.event_filters import (
     event_for_location,
@@ -406,10 +407,44 @@ class GenerateEvents:
 
             elif event.supplies:
                 clan_size = get_living_clan_cat_count(Cat_class)
+
+                # finding cats with the CAMP skill
+                camp_cats = [
+                    c
+                    for c in Cat_class.all_cats_list
+                    if c.status.alive_in_player_clan
+                    and (
+                        (c.skills.primary and c.skills.primary.path == SkillPath.CAMP)
+                        or (
+                            c.skills.secondary
+                            and c.skills.secondary.path == SkillPath.CAMP
+                        )
+                    )
+                ]
+
+                avoidance_chance = 1
+                # each camp cat will increase the chance that significant reduction events do not occur
+                for c in camp_cats:
+                    # tiers are added in order to make the chance num, this means the higher tiers have greater influence
+                    if c.skills.primary.path == SkillPath.CAMP:
+                        # +1 bc primary paths should have a little bit larger influence
+                        avoidance_chance += c.skills.primary.tier + 1
+                    elif (
+                        c.skills.secondary and c.skills.secondary.path == SkillPath.CAMP
+                    ):
+                        avoidance_chance += c.skills.secondary.tier
+
                 discard = False
                 for supply in event.supplies:
                     trigger = supply["trigger"]
                     supply_type = supply["type"]
+
+                    if (
+                        supply["adjust"] in ["reduce_half", "reduce_full"]
+                        and random.randint(1, avoidance_chance) != 1
+                    ):
+                        continue
+
                     if supply_type == "freshkill":
                         if not freshkill_active:
                             continue
@@ -542,26 +577,20 @@ class GenerateEvents:
     @staticmethod
     def possible_death_reactions(family_relation, rel_value, trait, body_status):
         possible_events = []
-
-        # grab general events first, since they'll usually exist
+        # grab general events first, since they'll always exist
         events = GenerateEvents.get_death_reaction_dicts("general", rel_value)
-        if events:
-            if "general" in events and body_status in events["general"]:
-                possible_events.extend(events["general"][body_status])
-            if trait in events and body_status in events[trait]:
-                possible_events.extend(events[trait][body_status])
+        possible_events.extend(events["general"][body_status])
+        if trait in events and body_status in events[trait]:
+            possible_events.extend(events[trait][body_status])
 
         # grab family events if they're needed. Family events should not be romantic.
         if family_relation != "general" and rel_value != RelType.ROMANCE:
             events = GenerateEvents.get_death_reaction_dicts(family_relation, rel_value)
-            if events:
-                if "general" in events and body_status in events["general"]:
-                    possible_events.extend(events["general"][body_status])
-                if trait in events and body_status in events[trait]:
-                    possible_events.extend(events[trait][body_status])
+            possible_events.extend(events["general"][body_status])
+            if trait in events and body_status in events[trait]:
+                possible_events.extend(events[trait][body_status])
 
         return possible_events
-
 
     def possible_lead_den_events(
         self,
