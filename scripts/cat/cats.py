7392f927988045cb0f5fe38a19759a5caefda4a2
Arcticnightsky <fralow.msn@gmail.com>
2025-09-25 18:24:20 -0400

    Update cats.py

--- scripts/cat/cats.py
+++ scripts/cat/cats.py
@@ -415,7 +415,6 @@ class Cat:
 
         # APPEARANCE
         self.pelt = Pelt.generate_new_pelt(
-            self.gender,
             [Cat.fetch_cat(i) for i in (self.parent1, self.parent2) if i],
             self.age,
         )
