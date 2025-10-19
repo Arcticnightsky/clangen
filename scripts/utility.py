ed9b49a7b36a855dec8ee039137be172987df8c5
Arcticnightsky <fralow.msn@gmail.com>
2025-10-19 00:32:18 -0400

    Update utility.py

--- scripts/utility.py
+++ scripts/utility.py
@@ -965,7 +965,7 @@ def create_new_cat(
                 deaf_chance = None
                 if (new_cat.pelt.colour == "WHITE" or new_cat.pelt.white_patches == "FULLWHITE") and new_cat.pelt.eye_colour in blue_eyes:
                     deaf_chance = constants.CONFIG["cat_generation"]["base_permanent_condition"] * 0.4
-                elif (new_cat.pelt.colour == "WHITE" or new_cat.pelt.white_patches == "FULLWHITE") and new_cat.eye_colour2 and new_cat.pelt.eye_colour2 in blue_eyes:
+                elif (new_cat.pelt.colour == "WHITE" or new_cat.pelt.white_patches == "FULLWHITE") and new_cat.pelt.eye_colour2 and new_cat.pelt.eye_colour2 in blue_eyes:
                     deaf_chance = constants.CONFIG["cat_generation"]["base_permanent_condition"] * 0.7
 
                 if deaf_chance:
