2b2957b2c1e4bb169f94959cafc7d5b0fbc6ec0f
Arcticnightsky <fralow.msn@gmail.com>
2025-12-23 20:11:47 -0500

    Update events.py

--- scripts/events.py
+++ scripts/events.py
@@ -1285,7 +1285,7 @@ def perform_ceremonies(cat):
                     chance = int(chance / 2.5)
 
                 if covered < len(relevant_cats):
-                    chance = int(chance / 4)
+                    chance = int(chance / 2.7)
                 
                 if cat.personality.trait in [
                     "careful",
