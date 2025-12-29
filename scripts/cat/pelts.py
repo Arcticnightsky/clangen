5e6bca2ec1dfd8d3c5970ba628b62b15dfe75bb2
Arcticnightsky <fralow.msn@gmail.com>
2025-12-28 23:35:27 -0500

    Genetic Accuracy

--- scripts/cat/pelts.py
+++ scripts/cat/pelts.py
@@ -662,21 +662,27 @@ class Pelt:
         for p_ in par_peltcolours:
             if p_ in Pelt.ginger_colours:
                 if gender == "female":
-                    add_weight = (30, 0, 0, 25)
+                    add_weight = (10, 0, 0, 25)
                 else:
                     add_weight = (40, 0, 0, 10)
             elif p_ in Pelt.black_colours:
-                add_weight = (0, 40, 2, 5)
+                if gender == "female":
+                    add_weight = (0, 15, 0, 20)
+                else:
+                    add_weight = (0, 40, 2, 5)
             elif p_ in Pelt.white_colours:
-                add_weight = (0, 5, 40, 0)
+                if gender == "female":
+                    add_weight = (0, 2, 40, 0)
+                else:
+                    add_weight = (0, 5, 40, 0)
             elif p_ in Pelt.brown_colours:
                 if gender == "female":
-                    add_weight = (3, 5, 0, 35)
+                    add_weight = (3, 3, 0, 35)
                 else:
                     add_weight = (10, 5, 0, 35)
             elif p_ is None:
                 if gender == "female":
-                    add_weight = (15, 40, 40, 40)
+                    add_weight = (8, 10, 40, 40)
                 else:
                     add_weight = (40, 40, 40, 40)
             else:
