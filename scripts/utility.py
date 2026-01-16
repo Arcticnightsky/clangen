7b7e23ae2e1b36e52c20a5806486ac995d27f0d0
Arcticnightsky <fralow.msn@gmail.com>
2026-01-16 16:51:46 -0500

    Update utility.py

--- scripts/utility.py
+++ scripts/utility.py
@@ -401,7 +401,7 @@ def create_new_cat_block(
             break
 
         if match.group(1) == "has_kits":
-            age = randint(19, 120)
+            age = randint(20, 120)
             break
 
     if rank and not age:
@@ -532,7 +532,7 @@ def create_new_cat_block(
     chosen_cat: Optional["Cat"] = None
     if "exists" in attribute_list:
         existing_outsiders = [
-            i for i in Cat.all_cats.values() if i.status.is_outsider and not i.dead
+            i for i in Cat.all_cats.values() if i.status.is_outsider and not i.dead and not entry["near"] for entry in i.standing_history
         ]
         possible_outsiders = []
         for cat in existing_outsiders:
