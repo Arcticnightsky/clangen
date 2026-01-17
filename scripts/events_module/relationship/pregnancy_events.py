dec833299d47c4e0c9fdfeeef0587b1620c2e7a9
Arcticnightsky <fralow.msn@gmail.com>
2026-01-17 01:12:17 -0500

    Update pregnancy_events.py

--- scripts/events_module/relationship/pregnancy_events.py
+++ scripts/events_module/relationship/pregnancy_events.py
@@ -193,7 +193,7 @@ class Pregnancy_Events:
         for kit in kits:
             kit.thought = "Snuggles close to r_c"
             kit.thought = event_text_adjust(Cat, kit.thought, random_cat=cat)
-            cats_involved = [kit.ID]
+            cats_involved = [cat.ID] + [kit.ID for kit in kits]
 
         # Normally, birth cooldown is only applied to cat who gave birth
         # However, if we don't apply birth cooldown to adoption, we get
