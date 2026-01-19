7a0881f17070753fb2e63a736e7e6d5c7540fddc
Arcticnightsky <fralow.msn@gmail.com>
2026-01-19 14:14:03 -0500

    Update relation_events.py

--- scripts/events_module/relationship/relation_events.py
+++ scripts/events_module/relationship/relation_events.py
@@ -96,7 +96,7 @@ class Relation_Events:
                 continue
                 
             if cat.gender == inter_cat.gender and random_module.randint(1, 2500) != 1:
-                print(f"No gay shit for {cat} and {inter_cat} today")
+                print(f"No gay shit for {cat.name} and {inter_cat.name} today")
                 continue # balancing same-sex relationships - there are too many and I just want more kits in my clans, sorry >:(
             
             if inter_cat.ID not in cat.relationships:
