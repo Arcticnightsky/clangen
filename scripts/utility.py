9587e1a53f9938e0ff56e11997a6bfd5aa903b9c
Arcticnightsky <fralow.msn@gmail.com>
2026-01-19 15:34:42 -0500

    Update utility.py

--- scripts/utility.py
+++ scripts/utility.py
@@ -1638,9 +1638,9 @@ def change_relationship_values(
                 single_cat_from.is_potential_mate(single_cat_to, for_love_interest=True)
                 or single_cat_to.ID in single_cat_from.mate
             ):
-                if single_cat_from.gender == single_cat_to.gender and single_cat_to.ID not in single_cat_from.mate and random_module.randint(1, 2500) != 1:
+                if single_cat_from.gender == single_cat_to.gender and single_cat_to.ID not in single_cat_from.mate and random_module.randint(1, 5500) != 1:
                     print(f"No gay shit for {single_cat_from.name} and {single_cat_to.name} today")
-                    return False # balancing same-sex relationships - there are too many and I just want more kits in my clans, sorry >:(
+                    continue # balancing same-sex relationships - there are too many and I just want more kits in my clans, sorry >:(
                 elif single_cat_from.gender != single_cat_to.gender:
                     # now gain the romance
                     rel.romance += romance
