a1cd9f6b325afac6f76bc007c429abef84165204
Arcticnightsky <fralow.msn@gmail.com>
2026-01-27 11:49:27 -0500

    Update romantic_events.py

--- scripts/events_module/relationship/romantic_events.py
+++ scripts/events_module/relationship/romantic_events.py
@@ -181,7 +181,6 @@ class RomanticEvents:
             return False
 
         if cat_from.gender == cat_to.gender and cat_to.ID not in cat_from.mate and random_module.randint(1, 5500) != 1:
-            print(f"No gay shit for {cat_from.name} and {cat_to.name} today")
             return False # balancing same-sex relationships - there are too many and I just want more kits in my clans, sorry >:(
         
         # chose interaction
