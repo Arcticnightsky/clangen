0a996c4dd7390ddb845d955ca4285514438a08be
Arcticnightsky <fralow.msn@gmail.com>
2026-01-31 11:48:01 -0500

    Update ProfileScreen.py

--- scripts/screens/ProfileScreen.py
+++ scripts/screens/ProfileScreen.py
@@ -745,12 +745,13 @@ class ProfileScreen(Screens):
         output += "\n"
 
         # PELT TYPE
-        output += i18n.t(
-            "screens.profile.pelt_label",
-            pelt=i18n.t(f"cat.pelts.{the_cat.pelt.name}").lower(),
-        )
         if the_cat.pelt.white_patches == "FULLWHITE":
-            output += " with full-white spotting " 
+            output += "fullwhite" 
+        else:
+            output += i18n.t(
+                "screens.profile.pelt_label",
+                pelt=i18n.t(f"cat.pelts.{the_cat.pelt.name}").lower(),
+            )
         # NEWLINE ----------
         output += "\n"
 
