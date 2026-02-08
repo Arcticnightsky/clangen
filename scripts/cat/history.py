86d580dded85fb1d348003d6ab05db2f771b34e4
Arcticnightsky <fralow.msn@gmail.com>
2026-02-07 19:36:06 -0500

    Update history.py

--- scripts/cat/history.py
+++ scripts/cat/history.py
@@ -543,11 +543,11 @@ class History:
                     murder["revealed"]["to_clan"] = True
                 else:
                     murder["revealed"]["aware_individuals"].extend(aware_individuals)
-                    # making double reveals happen, ex: cat1 knows about cat2 murdering cat3 and later on decides to reveal cat2's murder to the clan
-                    if aware_individuals and murder["revealed"]["to_clan"] == False:
-                        if murder["victim"] == victim.ID:
-                            if clan_reveal:
-                                murder["revealed"]["to_clan"] = True
+        # making double reveals happen, ex: cat1 knows about cat2 murdering cat3 and later on decides to reveal cat2's murder to the clan
+        if aware_individuals and self.murder["is_murderer"]["revealed"]["to_clan"] == False:
+            if self.murder["is_murderer"]["victim"] == victim.ID:
+                if clan_reveal:
+                    self.murder["is_murderer"]["revealed"]["to_clan"] = True
         
         for murder in victim.history.murder["is_victim"]:
             if murder["murderer"] == murderer_id:
