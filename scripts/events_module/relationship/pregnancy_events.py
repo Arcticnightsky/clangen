a9275c2f32202c45465e9a32cf2a32c9aef5e810
Arctic <fralow.msn@gmail.com>
2026-02-11 22:00:19 -0500

    Revert "Update pregnancy_events.py"
    This reverts commit ba8155006f2b60cd05fe2a5108909a1def844756.

--- scripts/events_module/relationship/pregnancy_events.py
+++ scripts/events_module/relationship/pregnancy_events.py
@@ -1391,6 +1391,8 @@ class Pregnancy_Events:
         )
         if living_cats < 10:
             inverse_chance = int(inverse_chance * 0.5)
+        elif living_cats > 30:
+            inverse_chance = int(inverse_chance * (living_cats / 30))
 
         # COMPATIBILITY
         # - decrease / increase depending on the compatibility
