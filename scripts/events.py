de754b4b3d463bac35f86cc90f21a5e093ef5e3f
Arcticnightsky <fralow.msn@gmail.com>
2026-02-13 19:35:14 -0500

    Update events.py

--- scripts/events.py
+++ scripts/events.py
@@ -457,6 +457,7 @@ def handle_lead_den_event():
         # ADJUST REP
         game.clan.reputation += chosen_event["rep_change"]
 
+        additional_mates = None
         additional_kits = None
         # SUCCESS/FAIL
         if info_dict["success"]:
@@ -476,19 +477,36 @@ def handle_lead_den_event():
             elif info_dict["interaction_type"] in ("invite", "search"):
                 # ADD TO CLAN AND CHECK FOR KITS
                 additional_kits = outsider_cat.add_to_clan()
-
+                for mate_id in outsider_cat.mate:
+                    mate = Cat.all_cats.get(mate_id)
+                    if (
+                        mate
+                        and mate.status.is_outsider
+                        and not mate.dead
+                        and not CatStanding.EXILED
+                    ):
+                        additional_mates.append(mate)
+                        additional_mates = outsider_cat.add_to_clan()
+                
                 if additional_kits:
                     event_text += i18n.t(
                         "hardcoded.event_lost_kits", count=len(additional_kits)
                     )
-
+                    
                     for kit_ID in additional_kits:
                         # add to involved cat list
                         involved_cats.append(kit_ID)
 
+                if additional_mates:
+                    event_text += i18n.t("hardcoded.event_lost_mate")
+                    
+                    for mate_id in lost_cat.mate:
+                            # add to involved cat list
+                        involved_cats.append(mate_id)
+                
                 invited_cats = [outsider_cat.ID]
                 invited_cats.extend(additional_kits)
-
+                invited_cats.extend(additional_mates)
                 invited_cats = [outsider_cat.ID]
                 
                 for cat_ID in invited_cats:
