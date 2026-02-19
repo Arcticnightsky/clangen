bf47413c5697f71edd65e4e17d68cc7f71eb5892
Arcticnightsky <fralow.msn@gmail.com>
2026-02-18 19:45:43 -0500

    Update status.py

--- scripts/cat/status.py
+++ scripts/cat/status.py
@@ -570,7 +570,7 @@ class Status:
         """
         # if we have an outsider who has never been a clancat, they go to the unknown residence
         if self.is_outsider and (
-           not self.is_exiled(CatGroup.PLAYER_CLAN_ID) or not cat.status.is_lost(CatGroup.PLAYER_CLAN_ID) or not self.is_former_clancat
+           not self.is_exiled(CatGroup.PLAYER_CLAN_ID) or not cat.is_lost(CatGroup.PLAYER_CLAN_ID) or not self.is_former_clancat
         ):
             return CatGroup.UNKNOWN_RESIDENCE_ID
 
@@ -578,7 +578,7 @@ class Status:
         if self.is_exiled(CatGroup.PLAYER_CLAN_ID):
             return CatGroup.DARK_FOREST_ID
 
-        if self.status.is_lost(CatGroup.PLAYER_CLAN_ID):
+        if self.is_lost(CatGroup.PLAYER_CLAN_ID):
             return CatGroup.STARCLAN_ID
         # meanwhile clan cats go wherever their guide points them
         if game.clan:
