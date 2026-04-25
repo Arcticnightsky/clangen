b6e6f222895165040a426162f9f7cfe34bb3b0d1
Arcticnightsky <fralow.msn@gmail.com>
2026-04-25 15:02:31 -0400

    Block patrols for queens caring for kits <=5 moons

--- scripts/screens/PatrolScreen.py
+++ scripts/screens/PatrolScreen.py
@@ -17,6 +17,7 @@ from ..ui.scale import ui_scale, ui_scale_dimensions
 from .Screens import Screens
 from .enums import GameScreen
 from ..clan_package.settings import get_clan_setting
+from ..clan_package.get_clan_cats import get_queens_with_young_kits
 from ..game_structure import image_cache, constants
 from ..game_structure.game.switches import switch_set_value, Switch
 from ..game_structure.game.settings import game_setting_get
@@ -1010,6 +1011,10 @@ class PatrolScreen(Screens):
 
         self.able_cats = []
 
+        queens_with_young_kits = get_queens_with_young_kits(
+            Cat.all_cats_list, max_kit_moons=5
+        )
+
         # ASSIGN TO ABLE CATS
         for the_cat in Cat.all_cats_list:
             if (
@@ -1019,6 +1024,7 @@ class PatrolScreen(Screens):
                 and the_cat.status.alive_in_player_clan
                 and the_cat not in self.current_patrol
                 and not the_cat.not_working()
+                and the_cat.ID not in queens_with_young_kits
             ):
                 if (
                     the_cat.status.rank == CatRank.NEWBORN
