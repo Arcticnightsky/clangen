24f23e9ecc6996606aa875df8d5c8d047ecdeb5e
Arcticnightsky <fralow.msn@gmail.com>
2026-05-28 00:47:29 -0400

    Remove unnecessary addition in pregnancy_events.py

--- scripts/events_module/relationship/pregnancy_events.py
+++ scripts/events_module/relationship/pregnancy_events.py
@@ -1802,7 +1802,6 @@ class Pregnancy_Events:
                 start_relation = Relationship(kitten, second_kitten, False, True)
                 start_relation.romance += (
                     constants.CONFIG["new_cat"]["sib_buff"]["cat1_to_cat2"]["romance"]
-                    + y
                 )
                 start_relation.like += (
                     constants.CONFIG["new_cat"]["sib_buff"]["cat1_to_cat2"]["like"] + y
