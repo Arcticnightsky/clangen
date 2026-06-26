f23311754927ec7e635c550f724f92c7cb292287
Arctic <171509110+Arcticnightsky@users.noreply.github.com>
2026-06-26 14:25:56 -0400

    Revert "Format enemy clan name with prefix"
    This reverts commit aee48a47a66111f7abd8df10dc6fbe999de04115.

--- scripts/events.py
+++ scripts/events.py
@@ -1367,7 +1367,7 @@ def check_war():
     event = ongoing_event_text_adjust(
         Cat,
         event,
-        other_clan_name=f"{enemy_clan.prefix}Clan",
+        other_clan_name=enemy_clan.name,
         clan=game.clan,
     )
     game.cur_events_list.append(Single_Event(event, "other_clans"))
