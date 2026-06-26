aee48a47a66111f7abd8df10dc6fbe999de04115
Arcticnightsky <171509110+Arcticnightsky@users.noreply.github.com>
2026-06-26 13:51:31 -0400

    Format enemy clan name with prefix

--- scripts/events.py
+++ scripts/events.py
@@ -1367,7 +1367,7 @@ def check_war():
     event = ongoing_event_text_adjust(
         Cat,
         event,
-        other_clan_name=enemy_clan.name,
+        other_clan_name=f"{enemy_clan.prefix}Clan",
         clan=game.clan,
     )
     game.cur_events_list.append(Single_Event(event, "other_clans"))
