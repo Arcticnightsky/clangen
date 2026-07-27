cbcfa74973b5dec058b2b5d49195affc62603dc9
Arcticnightsky <171509110+Arcticnightsky@users.noreply.github.com>
2026-07-27 14:03:15 -0400

    Fix translation output for relation display

--- scripts/ui/windows/relationship_log.py
+++ scripts/ui/windows/relationship_log.py
@@ -447,7 +447,7 @@ class RelationshipLogWindow(GameWindow):
             if relation:
                 output += f"{i18n.t('windows.relation_connection', relation=relation)}"
                 if relation == f"{i18n.t('general.has_a_mate')}<br>":
-                    output = f"{i18n.t(relation=relation)}"
+                    output = f"{i18n.t(relation)}"
 
         return output
 
