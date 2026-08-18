83999712f4827c0a021f13c6c9cbab0aeb4462f5
Arcticnightsky <171509110+Arcticnightsky@users.noreply.github.com>
2026-08-18 18:09:00 -0400

    Refactor names loading and cleanup code
    Removed normalization of names dictionary and fixed file read path.

--- scripts/cat/names.py
+++ scripts/cat/names.py
@@ -5,7 +5,6 @@ Module that handles the name generation for all cats.
 import contextlib
 import os
 import random
-from copy import deepcopy
 
 import i18n
 import ujson
@@ -139,8 +138,6 @@ class Name:
         else:
             names_dict = load_lang_resource("names.json")
 
-        names_dict = self._normalize_names_dict(names_dict)
-
         save_dir = get_save_dir()
 
         # here onwards is copied wholesale from the original Name class
@@ -179,7 +176,7 @@ class Name:
 
         if os.path.exists(save_dir + "/specialsuffixes.txt"):
             with open(
-                str(save_dir + "/specialsuffixes.txt"), "r", encoding="utf-8"
+                str(save_dir + "/specialsuffixes.txt", "r"), encoding="utf-8"
             ) as read_file:
                 name_list = read_file.read()
                 if_names = len(name_list)
@@ -188,7 +185,7 @@ class Name:
                 for new_name in new_names:
                     if new_name != "":
                         if new_name.startswith("-"):
-                            names_dict["special_suffixes"].pop(new_name[1:], None)
+                            del names_dict["special_suffixes"][new_name[1:]]
                         elif ":" in new_name:
                             _tmp = new_name.split(":")
                             names_dict["special_suffixes"][_tmp[0]] = _tmp[1]
